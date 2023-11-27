4e4c30daabfe021bfcb07c418bc376f3d25780db
fatimazahraelhoussni <fatimazahra.elhoussni20@ump.ac.ma>
2023-11-28 00:15:42 +0100

    sign up with firebase

--- app/src/main/java/com/example/chatapplication/activities/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.chatapplication;
+package com.example.chatapplication.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.chatapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
